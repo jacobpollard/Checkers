887ec7e1e9e7e8dd8309b228e0866776d13152fe
Neil <butcherna@appstate.edu>
2015-02-03 13:51:51 -0500

    Wrote Pieces Class
    Pushin pieces

--- Pieces.java
+++ Pieces.java
@@ -1,1 +1,14 @@
 public class Pieces
+{
+	private boolean isKing;
+	private boolean CompPiece;
+	public Pieces(boolean c)
+	{
+		CompPiece = c;
+		isKing = false;	
+	}
+	public void KingMe()
+	{
+		isKing = true;
+	}
+}	
