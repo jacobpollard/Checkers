eaad83fc46c6280ca6efd1bf37372d7830deb133
jacobpollard <jakerspollard@gmail.com>
2015-02-09 21:17:53 -0500

    Commit #3

--- Game.java
+++ Game.java
@@ -1,3 +1,47 @@
-/*
-*Noob
-*/
+import java.util.LinkedList;
+public class Game
+{
+    static Game uniqueInstance;
+    CheckerBoard board;
+    boolean playerTurn;
+    LinkedList<Pieces> playerJumps;
+    LinkedList<Pieces> compJumps;
+    private Game()
+    {
+        playerJumps = new LinkedList<Pieces>();
+        compJumps = new LinkedList<Pieces>();
+        board = new CheckerBoard();
+        playerTurn = false;
+    }
+    static Game getInstance()
+    {
+        if(uniqueInstance == null)
+        {
+            uniqueInstance = new Game();
+        }
+        return uniqueInstance;
+    }
+    
+    public boolean checkMove(int startX, int startY, int endX, int endY)
+    {
+        return( playerTurn && 
+              ( isJump(startX, startY, endX, endY) ||
+                isMove(startX, startY, endX, endY)));
+
+            
+        }
+    private boolean isMove(int startX, int startY, int endX, int endY)
+    {
+        if(playerTurn && board.hasPlayerPiece(startX, startY))
+        {
+            
+        }
+    }
+    private boolean isJump(int startX, int startY, int endX, int endY)
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+            
+    }
+}
--- Pieces.java
+++ Pieces.java
@@ -2,20 +2,25 @@ public class Pieces
 {
 	private boolean isKing;
 	private boolean CompPiece;
+	
 	public Pieces(boolean c)
 	{
 		CompPiece = c;
 		isKing = false;	
 	}
+	
 	public void KingMe()
 	{
 		isKing = true;
 	}
-}	public boolean isKing()
+	
+	public boolean isKing()
 	{
 		return isKing;
 	}
+	
 	public boolean isCompPiece()
 	{
 		return CompPiece;
 	}
+}
